35d8ed4ef55dfc6fa46854a71940d9570d8912cf
YuZhongze2 <2677807465@qq.com>
2024-12-07 14:34:40 +0800

    修复日志占用错误

--- backend/logging_config.py
+++ backend/logging_config.py
@@ -1,7 +1,6 @@
 import logging
 import os
-from logging.handlers import RotatingFileHandler, QueueHandler, QueueListener
-import queue
+from logging.handlers import TimedRotatingFileHandler
 
 class SocketIOHandler(logging.Handler):
     """自定义日志处理器，通过SocketIO发送日志消息。"""
@@ -14,36 +13,29 @@ class SocketIOHandler(logging.Handler):
         self.socketio.emit('log', {'message': log_entry})
 
 def configure_logging(app, socketio):
-    # 创建日志队列
-    log_queue = queue.Queue()
-
-    # 创建logs目录，如果不存在的话
     if not os.path.exists('logs'):
         os.makedirs('logs')
 
-    # 文件日志处理器，使用RotatingFileHandler
-    log_file_path = 'logs/app.log'
-    file_handler = RotatingFileHandler(log_file_path, maxBytes=10240, backupCount=10)
+    # 使用基于时间的日志轮换
+    file_handler = TimedRotatingFileHandler(
+        'logs/app.log', 
+        when='midnight',  # 每天午夜进行轮换
+        interval=1,       # 间隔1天
+        backupCount=10    # 保留最近10个日志文件
+    )
     formatter = logging.Formatter('%(asctime)s %(levelname)s: %(message)s [in %(pathname)s:%(lineno)d]')
     file_handler.setFormatter(formatter)
     file_handler.setLevel(logging.INFO)
 
-    # 创建QueueHandler，将日志放入队列
-    queue_handler = QueueHandler(log_queue)
-
-    # SocketIO日志处理器
+    # 自定义 SocketIO 日志处理器
     socketio_handler = SocketIOHandler(socketio)
     socketio_handler.setFormatter(formatter)
     socketio_handler.setLevel(logging.INFO)
 
-    # 添加队列处理器和SocketIO处理器
-    app.logger.addHandler(queue_handler)
+    # 将文件和 SocketIO 日志处理器添加到应用的日志系统中
+    app.logger.addHandler(file_handler)
     app.logger.addHandler(socketio_handler)
 
-    # 使用QueueListener来监听日志队列并将日志写入文件
-    listener = QueueListener(log_queue, file_handler)
-    listener.start()
-
-    # 设置日志级别
+    # 设置日志记录级别为 INFO
     app.logger.setLevel(logging.INFO)
     app.logger.info('Wind Forecast Backend Startup')
